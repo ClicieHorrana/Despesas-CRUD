5cdfbc0be1b4ecced59e416fb84f5d913999ca88
Your Name <you@example.com>
2024-08-30 22:05:37 -0300

    BugFix/ ajuste de nomeações

--- app/Http/Requests/ExpenseRequest.php
+++ app/Http/Requests/ExpenseRequest.php
@@ -20,7 +20,7 @@ class ExpenseRequest extends FormRequest
             "description" => "required|string|max:191",
             "user_id" => "required|integer|exists:users,id",
             "amount" => "required|numeric|min:0",
-            'date_expense' => "nullable|date|before_or_equal:now|date_format:Y-m-d"
+            'expense_date' => "nullable|date|before_or_equal:now|date_format:Y-m-d"
         ];
     }
 
@@ -28,8 +28,8 @@ class ExpenseRequest extends FormRequest
     {
         return [
             'description.required' => 'A descrição da despesa é obrigatória.',
-            'date_expense.required' => 'A data da despesa é obrigatória.',
-            'date_expense.before_or_equal' => 'A data não pode ser no futuro.',
+            'expense_date.required' => 'A data da despesa é obrigatória.',
+            'expense_date.before_or_equal' => 'A data não pode ser no futuro.',
             'amount.required' => 'O valor da despesa é obrigatório.',
             'amount.min' => 'O valor da despesa não pode ser negativo.',
             'user_id' => 'O autor/responsável da despesa é obrigatório.',
